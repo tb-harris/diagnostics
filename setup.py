73f07ee9020a8d1cd85ec237f2d7a4adba3d4da2
Trevor Brandon-Harris <tbharris@ucdavis.edu>
2024-07-09 09:44:01 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import diagnostics
+
+setup(name='Diagnostics',
+      version='1.0',
+      description='Packages the linear regression diagnostics from statsmodels examples',
+      author='Trevor Brandon-Harris',
+      author_email='tbrandonharris@riverdale.edu',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['diagnostics'],
+     )
